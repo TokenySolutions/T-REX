bbd3074f40727d0c4b99983b89a97765674aba77
Dilip Chouhan <dilip@tokeny.com>
2019-01-04 13:58:18 +0530

    Created an example for compliance rule to limit holder count

---
+++ contracts/compliance/LimitHolder.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.24;
+
+import "./ICompliance.sol";
+import "../token/Token.sol";
+
+contract LimitHolder is Compliance {
+    Token token;
+    uint holderLimit;
+
+
+    constructor (address _token, uint _holderLimit) public {
+        token = Token(_token);
+        holderLimit = _holderLimit;
+    }
+
+    function getHolderCount() public view returns(uint) {
+        return token.holderCount();
+    }
+
+    function canTransfer(address _from, address _to, uint256 _value) public view returns (bool) {
+        if(token.holderCount() < holderLimit) {
+            return true;
+        }
+        return false;
+    }
+}
