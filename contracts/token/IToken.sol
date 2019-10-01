db9ee1a8e49f6cc23ba2da717e667024add9cb28
Nakasar <kevin@thizy.eu>
2019-10-01 13:57:58 +0200

    fix(IToken) Remove constant declaration from interface

--- contracts/token/IToken.sol
+++ contracts/token/IToken.sol
@@ -5,7 +5,7 @@ contract IToken{
 
     string public name;
     string public symbol;
-    uint8 public constant decimals;
+    uint8 public decimals;
     string public version;
 
     event UpdatedTokenInformation(string newName, string newSymbol, string newVersion);
