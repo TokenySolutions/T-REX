f268146d2616c6c89085cb62424aad62de4ab8db
Kevin Thizy <kevin.thizy@intech.lu>
2019-11-27 10:23:14 +0100

    ♻(IdentityRegistry) Use Interface

--- contracts/registry/IIdentityRegistry.sol
+++ contracts/registry/IIdentityRegistry.sol
@@ -7,25 +7,29 @@ import "@onchain-id/solidity/contracts/ClaimIssuer.sol";
 import "@onchain-id/solidity/contracts/Identity.sol";
 
 interface IIdentityRegistry {
-
+    // EVENTS
+    event ClaimTopicsRegistrySet(address indexed _claimTopicsRegistry);
+    event CountryUpdated(address indexed investorAddress, uint16 indexed country);
     event IdentityRegistered(address indexed investorAddress, Identity indexed identity);
     event IdentityRemoved(address indexed investorAddress, Identity indexed identity);
     event IdentityUpdated(Identity indexed old_identity, Identity indexed new_identity);
-    event CountryUpdated(address indexed investorAddress, uint16 indexed country);
-    event ClaimTopicsRegistrySet(address indexed _claimTopicsRegistry);
     event TrustedIssuersRegistrySet(address indexed _trustedIssuersRegistry);
 
-    function registerIdentity(address _user, Identity _identity, uint16 _country) external;
-    function updateIdentity(address _user, Identity _identity) external;
-    function updateCountry(address _user, uint16 _country) external;
+    // WRITE OPERATIONS
     function deleteIdentity(address _user) external;
-    function isVerified(address _userAddress) external view returns (bool);
+    function registerIdentity(address _user, Identity _identity, uint16 _country) external;
     function setClaimTopicsRegistry(address _claimTopicsRegistry) external;
     function setTrustedIssuersRegistry(address _trustedIssuersRegistry) external;
-    function contains(address _wallet) external view returns (bool);
-    function investorCountry(address _wallet) external view returns (uint16);
-    function identity(address _wallet) external view returns (Identity);
-    function topicsRegistry() external view returns (IClaimTopicsRegistry);
-    function issuersRegistry() external view returns (ITrustedIssuersRegistry);
+    function updateCountry(address _user, uint16 _country) external;
+    function updateIdentity(address _user, Identity _identity) external;
 
+    // READ OPERATIONS
+    function contains(address _wallet) external view returns (bool);
+    function isVerified(address _userAddress) external view returns (bool);
+
+    // GETTERS
+    function identity(address _wallet) external view returns (Identity);
+    function investorCountry(address _wallet) external view returns (uint16);
+    function issuersRegistry() external view returns (ITrustedIssuersRegistry);
+    function topicsRegistry() external view returns (IClaimTopicsRegistry);
 }
