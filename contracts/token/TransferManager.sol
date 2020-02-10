c49375c7d548a8704e396c47efa5cfdbeeb1bd69
Joachim <joachim@tokeny.com>
2020-02-10 11:10:01 +0100

    Update TransferManager.sol

--- contracts/token/TransferManager.sol
+++ contracts/token/TransferManager.sol
@@ -186,9 +186,9 @@ contract TransferManager is Pausable {
     
     /**
     * 
-    *  Require that the from and to addresses are not frozen.
-    *  Require that the value should not exceed available balance .
-    *  Require that the to address is a verified address,
+    *  Require that the from address has enough available tokens to transfer `value` amount if he has partial freeze on some tokens.
+    *  Require that the `value` should not exceed available balance.
+    *  Require that the `to` address is a verified address,
     *  If the `to` address is not currently a shareholder then it MUST become one.
     *  If the transfer will reduce `from`'s balance to 0 then that address
     *  MUST be removed from the list of shareholders.
@@ -200,8 +200,7 @@ contract TransferManager is Pausable {
     * @return `true` if successful and revert if unsuccessful
     */
     function forcedTransfer(address _from, address _to, uint256 _value) onlyAgent external returns (bool) {
-        require(!frozen[_to] && !frozen[_from]);
-        require(_value <=  balanceOf(_from).sub(freezedTokens[_from]), "Sender Has Insufficient Balance" );
+        require(_value <=  balanceOf(_from).sub(freezedTokens[_from]), "Sender Has Insufficient Balance");
         if(identityRegistry.isVerified(_to) && compliance.canTransfer(_from, _to, _value)){
             updateShareholders(_to);
             pruneShareholders(_from, _value);
