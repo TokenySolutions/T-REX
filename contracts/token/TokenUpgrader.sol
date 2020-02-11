ca364ec1430db14fe440b0e8fc89fb2c8bf04184
Dilip Chouhan <dilip@tokeny.com>
2020-02-11 17:27:33 +0530

    Added UpgradeableToken and TokenUpgrader contracts

---
+++ contracts/token/TokenUpgrader.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+
+/**
+ * Token upgrader interface inspired by Lunyr.
+ *
+ * Token upgrader transfers previous version tokens to a newer version.
+ * Token upgrader itself can be the token contract, or just a middle man contract doing the heavy lifting.
+ */
+contract TokenUpgrader {
+    uint public originalSupply;	
+
+    /** Interface marker */
+    function isTokenUpgrader() external pure returns (bool) {
+        return true;
+    }
+
+    function upgradeFrom(address _from, uint256 _value) public;
+}
