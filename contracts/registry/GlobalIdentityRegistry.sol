0ded0e2aa0d0c20deaa578d75795da554d7271bf
joachimlebrun <joachim@tokeny.com>
2020-03-26 20:07:10 +0100

    ➕() global identity registry

---
+++ contracts/registry/GlobalIdentityRegistry.sol
@@ -0,0 +1,233 @@
+/**
+ *     NOTICE
+ *
+ *     The T-REX software is licensed under a proprietary license or the GPL v.3.
+ *     If you choose to receive it under the GPL v.3 license, the following applies:
+ *     T-REX is a suite of smart contracts developed by Tokeny to manage and transfer financial assets on the ethereum blockchain
+ *
+ *     Copyright (C) 2019, Tokeny sàrl.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pragma solidity ^0.6.0;
+
+
+import "@onchain-id/solidity/contracts/IClaimIssuer.sol";
+import "@onchain-id/solidity/contracts/IIdentity.sol";
+import "../registry/IClaimTopicsRegistry.sol";
+import "../registry/ITrustedIssuersRegistry.sol";
+import "../registry/IGlobalIdentityRegistry.sol";
+import "../roles/AgentRole.sol";
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract GlobalIdentityRegistry is IGlobalIdentityRegistry, AgentRole {
+    /// mapping between a user address and the corresponding identity contract
+    mapping(address => IIdentity) private identity;
+
+    /// mapping between a user address and its corresponding country
+    mapping(address => uint16) private investorCountry;
+
+    /// mapping between a token and its ClaimTopicsRegistry Contract
+    mapping(address => IClaimTopicsRegistry) private tokenTopicsRegistry;
+
+    /// mapping between a token and its TrustedIssuersRegistry Contract
+    mapping(address => ITrustedIssuersRegistry) private tokenIssuersRegistry;
+
+    constructor (
+        address _token,
+        address _trustedIssuersRegistry,
+        address _claimTopicsRegistry
+    ) public {
+        tokenTopicsRegistry[_token] = IClaimTopicsRegistry(_claimTopicsRegistry);
+        tokenIssuersRegistry[_token] = ITrustedIssuersRegistry(_trustedIssuersRegistry);
+
+        emit ClaimTopicsRegistrySet(_token, _claimTopicsRegistry);
+        emit TrustedIssuersRegistrySet(_token, _trustedIssuersRegistry);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-getIdentityOfWallet}.
+    */
+    function getIdentityOfWallet(address _userAddress) public override view returns (IIdentity){
+        return identity[_userAddress];
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-getInvestorCountryOfWallet}.
+    */
+    function getInvestorCountryOfWallet(address _userAddress) public override view returns (uint16){
+        return investorCountry[_userAddress];
+    }
+
+    /**
+     *  @dev Returns the TrustedIssuersRegistry linked to the current IdentityRegistry.
+     */
+    function issuersRegistry(address _token) public override view returns (ITrustedIssuersRegistry){
+        return tokenIssuersRegistry[_token];
+    }
+
+    /**
+     *  @dev Returns the ClaimTopicsRegistry linked to the current IdentityRegistry.
+     */
+    function topicsRegistry(address _token) public override view returns (IClaimTopicsRegistry){
+        return tokenTopicsRegistry[_token];
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-registerIdentity}.
+    */
+    function registerIdentity(address _userAddress, IIdentity _identity, uint16 _country) public override onlyAgent {
+        require(address(_identity) != address(0), "contract address can't be a zero address");
+        require(address(identity[_userAddress]) == address(0), "identity contract already exists, please use update");
+        identity[_userAddress] = _identity;
+        investorCountry[_userAddress] = _country;
+
+        emit IdentityRegistered(_userAddress, _identity);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-batchRegisterIdentity}.
+    */
+    function batchRegisterIdentity(address[] calldata __userAddresses, IIdentity[] calldata _identities, uint16[] calldata _countries) external override {
+        for (uint256 i = 0; i < __userAddresses.length; i++) {
+            registerIdentity(__userAddresses[i], _identities[i], _countries[i]);
+        }
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-updateIdentity}.
+    */
+    function updateIdentity(address _userAddress, IIdentity _identity) public override onlyAgent {
+        require(address(identity[_userAddress]) != address(0));
+        require(address(_identity) != address(0), "contract address can't be a zero address");
+        identity[_userAddress] = _identity;
+
+        emit IdentityUpdated(identity[_userAddress], _identity);
+    }
+
+
+    /**
+    *  @dev See {IIdentityRegistry-updateCountry}.
+    */
+    function updateCountry(address _userAddress, uint16 _country) public override onlyAgent {
+        require(address(identity[_userAddress]) != address(0));
+        investorCountry[_userAddress] = _country;
+
+        emit CountryUpdated(_userAddress, _country);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-deleteIdentity}.
+    */
+    function deleteIdentity(address _userAddress) public override onlyAgent {
+        require(address(identity[_userAddress]) != address(0), "you haven't registered an identity yet");
+        delete identity[_userAddress];
+
+        emit IdentityRemoved(_userAddress, identity[_userAddress]);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-isVerified}.
+    */
+    function isVerified(address _userAddress) public override view returns (bool) {
+        require(address(tokenTopicsRegistry[msg.sender]) != address(0), "no claim topics registry for this token");
+        require(address(tokenIssuersRegistry[msg.sender]) != address(0), "no trusted issuers registry for this token");
+        if (address(identity[_userAddress]) == address(0)) {
+            return false;
+        }
+        uint256[] memory claimTopics = tokenTopicsRegistry[msg.sender].getClaimTopics();
+        uint length = claimTopics.length;
+        if (length == 0) {
+            return true;
+        }
+
+        uint256 foundClaimTopic;
+        uint256 scheme;
+        address issuer;
+        bytes memory sig;
+        bytes memory data;
+        uint256 claimTopic;
+        for (claimTopic = 0; claimTopic < length; claimTopic++) {
+            bytes32[] memory claimIds = identity[_userAddress].getClaimIdsByTopic(claimTopics[claimTopic]);
+            if (claimIds.length == 0) {
+                return false;
+            }
+            for (uint j = 0; j < claimIds.length; j++) {
+                (foundClaimTopic, scheme, issuer, sig, data,) = identity[_userAddress].getClaim(claimIds[j]);
+                if (!tokenIssuersRegistry[msg.sender].isTrustedIssuer(issuer)) {
+                    return false;
+                }
+                if (!tokenIssuersRegistry[msg.sender].hasClaimTopic(issuer, claimTopics[claimTopic])) {
+                    return false;
+                }
+                if (!IClaimIssuer(issuer).isClaimValid(identity[_userAddress], claimTopics[claimTopic], sig, data)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-setClaimTopicsRegistry}.
+    */
+    function setClaimTopicsRegistry(address _token, address _claimTopicsRegistry) public override onlyOwner {
+        tokenTopicsRegistry[_token] = IClaimTopicsRegistry(_claimTopicsRegistry);
+
+        emit ClaimTopicsRegistrySet(_token, _claimTopicsRegistry);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-setTrustedIssuersRegistry}.
+    */
+    function setTrustedIssuersRegistry(address _token, address _trustedIssuersRegistry) public override onlyOwner {
+        tokenIssuersRegistry[_token] = ITrustedIssuersRegistry(_trustedIssuersRegistry);
+
+        emit TrustedIssuersRegistrySet(_token, _trustedIssuersRegistry);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-contains}.
+    */
+    function contains(address _userAddress) public override view returns (bool){
+        if (address(identity[_userAddress]) == address(0)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-transferOwnershipOnIdentityRegistryContract}.
+    */
+    function transferOwnershipOnIdentityRegistryContract(address _newOwner) external override onlyOwner {
+        transferOwnership(_newOwner);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-addAgentOnIdentityRegistryContract}.
+    */
+    function addAgentOnIdentityRegistryContract(address _agent) external override {
+        addAgent(_agent);
+    }
+
+    /**
+    *  @dev See {IIdentityRegistry-removeAgentOnIdentityRegistryContract}.
+    */
+    function removeAgentOnIdentityRegistryContract(address _agent) external override {
+        removeAgent(_agent);
+    }
+}
---
+++ contracts/registry/IGlobalIdentityRegistry.sol
@@ -0,0 +1,219 @@
+/**
+ *     NOTICE
+ *
+ *     The T-REX software is licensed under a proprietary license or the GPL v.3.
+ *     If you choose to receive it under the GPL v.3 license, the following applies:
+ *     T-REX is a suite of smart contracts developed by Tokeny to manage and transfer financial assets on the ethereum blockchain
+ *
+ *     Copyright (C) 2019, Tokeny sàrl.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pragma solidity ^0.6.0;
+
+import "../registry/ITrustedIssuersRegistry.sol";
+import "../registry/IClaimTopicsRegistry.sol";
+
+import "@onchain-id/solidity/contracts/IClaimIssuer.sol";
+import "@onchain-id/solidity/contracts/IIdentity.sol";
+
+interface IGlobalIdentityRegistry {
+
+    /**
+     *  this event is emitted when the ClaimTopicsRegistry has been set for the IdentityRegistry
+     *  the event is emitted by the IdentityRegistry constructor
+     *  `_claimTopicsRegistry` is the address of the Claim Topics Registry contract
+     */
+    event ClaimTopicsRegistrySet(address indexed token, address indexed claimTopicsRegistry);
+
+    /**
+     *  this event is emitted when the ClaimTopicsRegistry has been set for the IdentityRegistry
+     *  the event is emitted by the IdentityRegistry constructor
+     *  `_claimTopicsRegistry` is the address of the Claim Topics Registry contract
+     */
+    event TrustedIssuersRegistrySet(address indexed token, address indexed trustedIssuersRegistry);
+
+    /**
+    *  this event is emitted when an Identity is registered into the Identity Registry.
+    *  the event is emitted by the 'registerIdentity' function
+    *  `investorAddress` is the address of the investor's wallet
+    *  `identity` is the address of the Identity smart contract (onchainID)
+    */
+    event IdentityRegistered(address indexed investorAddress, IIdentity indexed identity);
+
+    /**
+    *  this event is emitted when an Identity is removed from the Identity Registry.
+    *  the event is emitted by the 'deleteIdentity' function
+    *  `investorAddress` is the address of the investor's wallet
+    *  `identity` is the address of the Identity smart contract (onchainID)
+    */
+    event IdentityRemoved(address indexed investorAddress, IIdentity indexed identity);
+
+    /**
+    *  this event is emitted when an Identity has been updated
+    *  the event is emitted by the 'updateIdentity' function
+    *  `oldIdentity` is the old Identity contract's address to update
+    *  `newIdentity` is the new Identity contract's
+    */
+    event IdentityUpdated(IIdentity indexed oldIdentity, IIdentity indexed newIdentity);
+
+    /**
+    *  this event is emitted when an Identity's country has been updated
+    *  the event is emitted by the 'updateCountry' function
+    *  `investorAddress` is the address on which the country has been updated
+    *  `country` is the numeric code (ISO 3166-1) of the new country
+    */
+    event CountryUpdated(address indexed investorAddress, uint16 indexed country);
+
+    /**
+    *  @notice Register an identity contract corresponding to a user address.
+    *  Requires that the user doesn't have an identity contract already registered.
+    *  Only _agent can call.
+    *  emits `IdentityRegistered` event
+    *  @param _userAddress The address of the user
+    *  @param _identity The address of the user's identity contract
+    *  @param _country The country of the investor
+    */
+    function registerIdentity(address _userAddress, IIdentity _identity, uint16 _country) external;
+
+    /**
+    *  @notice Removes an user from the identity registry.
+    *  Requires that the user have an identity contract already deployed that will be deleted.
+    *  Only _agent can call.
+    *  emits `IdentityRemoved` event
+    *  @param _userAddress The address of the user to be removed
+    */
+    function deleteIdentity(address _userAddress) external;
+
+    /**
+    *  @notice Replace the actual claimTopicsRegistry contract with a new one.
+    *  Only owner can call.
+    *  emits `ClaimTopicsRegistrySet` event
+    *  @param _claimTopicsRegistry The address of the new claim Topics Registry
+    */
+    function setClaimTopicsRegistry(address _token, address _claimTopicsRegistry) external;
+
+    /**
+    *  @notice Replace the actual trustedIssuersRegistry contract with a new one.
+    *  Only owner can call.
+    *  emits `TrustedIssuersRegistrySet` event
+    *  @param _trustedIssuersRegistry The address of the new Trusted Issuers Registry
+    */
+    function setTrustedIssuersRegistry(address _token, address _trustedIssuersRegistry) external;
+
+    /**
+    *  @notice Updates the country corresponding to a user address.
+    *  Requires that the user should have an identity contract already deployed that will be replaced.
+    *  Only _agent can call.
+    *  emits `CountryUpdated` event
+    *  @param _userAddress The address of the user
+    *  @param _country The new country of the user
+    */
+    function updateCountry(address _userAddress, uint16 _country) external;
+
+    /**
+    *  @notice Updates an identity contract corresponding to a user address.
+    *  Requires that the user address should be the owner of the identity contract.
+    *  Requires that the user should have an identity contract already deployed that will be replaced.
+    *  Only _agent can call.
+    *  emits `IdentityUpdated` event
+    *  @param _userAddress The address of the user
+    *  @param _identity The address of the user's new identity contract
+    */
+    function updateIdentity(address _userAddress, IIdentity _identity) external;
+
+    /**
+     *  @notice function allowing to register identities in batch
+     *  Only _agent can call this function.
+     *  Requires that none of the users has an identity contract already registered.
+     *
+     *  IMPORTANT : THIS TRANSACTION COULD EXCEED GAS LIMIT IF `_userAddresses.length` IS TOO HIGH,
+     *  USE WITH CARE OR YOU COULD LOSE TX FEES WITH AN "OUT OF GAS" TRANSACTION
+     *
+     *  @param _userAddresses The addresses of the users
+     *  @param _identities The addresses of the corresponding identity contracts
+     *  @param _countries The countries of the corresponding investors
+     *
+     */
+    function batchRegisterIdentity(address[] calldata _userAddresses, IIdentity[] calldata _identities, uint16[] calldata _countries) external;
+
+    /**
+    *  @notice This functions checks whether a wallet has its Identity registered or not
+    *  in the Identity Registry.
+    *
+    *  @param _userAddress The address of the user to be checked.
+    *
+    *  @return 'True' if the address is contained in the Identity Registry, 'false' if not.
+    */
+    function contains(address _userAddress) external view returns (bool);
+
+    /**
+    *  @notice This functions checks whether an identity contract
+    *  corresponding to the provided user address has the required claims or not based
+    *  on the security token.
+    *
+    *  @param _userAddress The address of the user to be verified.
+    *
+    *  @return 'True' if the address is verified, 'false' if not.
+    */
+    function isVerified(address _userAddress) external view returns (bool);
+
+    /**
+     *  @dev Returns the onchainID of an investor.
+     *  @param _userAddress The wallet of the investor
+     */
+    function getIdentityOfWallet(address _userAddress) external view returns (IIdentity);
+
+    /**
+     *  @dev Returns the country code of an investor.
+     *  @param _userAddress The wallet of the investor
+     */
+    function getInvestorCountryOfWallet(address _userAddress) external view returns (uint16);
+
+
+    /**
+     *  @dev Returns the TrustedIssuersRegistry linked to the current IdentityRegistry.
+     */
+    function issuersRegistry(address _token) external view returns (ITrustedIssuersRegistry);
+
+    /**
+     *  @dev Returns the ClaimTopicsRegistry linked to the current IdentityRegistry.
+     */
+    function topicsRegistry(address _token) external view returns (IClaimTopicsRegistry);
+
+    /**
+    *  @notice Transfers the Ownership of the Identity Registry to a new Owner.
+    *  Only owner can call.
+    *
+    *  @param _newOwner The new owner of this contract.
+    */
+    function transferOwnershipOnIdentityRegistryContract(address _newOwner) external;
+
+    /**
+    *  @notice Adds an address as _agent of the Identity Registry Contract.
+    *  Only owner can call.
+    *
+    *  @param _agent The _agent's address to add.
+    */
+    function addAgentOnIdentityRegistryContract(address _agent) external;
+
+    /**
+     *  @notice Removes an address from being _agent of the Identity Registry Contract.
+     *  Only owner can call.
+     *
+     *  @param _agent The _agent's address to remove.
+     */
+    function removeAgentOnIdentityRegistryContract(address _agent) external;
+}
