af04d3ff78aaee3d8713579c8251abf4626a1328
Adam Boudjemaa <boudjemaa.adam@gmail.com>
2020-04-07 16:04:19 +0200

    Update Contracts Interface (#69)
    * Update index.d.ts
    
    * Update index.js
    
    * Update package.json
    
    * Update package.json
    
    * Update .solhint.json
    
    * Update .solhint.json
    
    * ✅() check compilers
    
    * ✅() check compilers
    
    * 👷🏻‍♂️() Remove test step
    
    Co-authored-by: Kevin Thizy <kevin.thizy@intech.lu>

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const ClaimIssuer = require('./build/contracts/ClaimIssuer');
 const ClaimTopicsRegistry = require('./build/contracts/ClaimTopicsRegistry');
 const DefaultCompliance = require('./build/contracts/DefaultCompliance');
 const IdentityRegistry = require('./build/contracts/IdentityRegistry');
+const IdentityRegistryStorage = require('./build/contracts/IdentityRegistryStorage');
 const IClaimIssuer = require('./build/contracts/IClaimIssuer');
 const IClaimTopicsRegistry = require('./build/contracts/IClaimTopicsRegistry');
 const ICompliance = require('./build/contracts/ICompliance');
 const IIdentityRegistry = require('./build/contracts/IIdentityRegistry');
+const IIdentityRegistryStorage = require('./build/contracts/IIdentityRegistryStorage');
 const IToken = require('./build/contracts/IToken');
 const ITrustedIssuersRegistry = require('./build/contracts/ITrustedIssuersRegistry');
 const Token = require('./build/contracts/Token');
@@ -16,6 +18,7 @@ module.exports = {
     ClaimTopicsRegistry,
     DefaultCompliance,
     IdentityRegistry,
+    IdentityRegistryStorage,
     Token,
     TrustedIssuersRegistry,
   },
@@ -23,6 +26,7 @@ module.exports = {
     IClaimTopicsRegistry,
     ICompliance,
     IIdentityRegistry,
+    IIdentityRegistryStorage,
     IToken,
     ITrustedIssuersRegistry,
   },
