1136dde025bce15afacded8aaa74132de9ab8158
aboudjem <boudjemaa.adam@gmail.com>
2021-01-11 05:59:33 +0100

    ➕(Proxy) add Upgradeable version of Roles for Token

---
+++ contracts/roles/AgentRoleUpgradeable.sol
@@ -0,0 +1,55 @@
+/**
+ *     NOTICE
+ *
+ *     The T-REX software is licensed under a proprietary license or the GPL v.3.
+ *     If you choose to receive it under the GPL v.3 license, the following applies:
+ *     T-REX is a suite of smart contracts developed by Tokeny to manage and transfer financial assets on the ethereum blockchain
+ *
+ *     Copyright (C) 2019, Tokeny sàrl.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pragma solidity ^0.6.2;
+
+import './Roles.sol';
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+
+contract AgentRoleUpgradeable is OwnableUpgradeable {
+    using Roles for Roles.Role;
+
+    event AgentAdded(address indexed _agent);
+    event AgentRemoved(address indexed _agent);
+
+    Roles.Role private _agents;
+
+    modifier onlyAgent() {
+        require(isAgent(msg.sender), 'AgentRole: caller does not have the Agent role');
+        _;
+    }
+
+    function isAgent(address _agent) public view returns (bool) {
+        return _agents.has(_agent);
+    }
+
+    function addAgent(address _agent) public onlyOwner {
+        _agents.add(_agent);
+        emit AgentAdded(_agent);
+    }
+
+    function removeAgent(address _agent) public onlyOwner {
+        _agents.remove(_agent);
+        emit AgentRemoved(_agent);
+    }
+}
