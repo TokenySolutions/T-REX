41319899d7830e36f50f6bc64214671a5a52193b
Joachim <joachim@tokeny.com>
2021-04-09 15:30:29 +0200

    🚨() fix lint

--- contracts/proxy/TREXImplementationAuthority.sol
+++ contracts/proxy/TREXImplementationAuthority.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 import '../access/Ownable.sol';
-import "./ITREXImplementationAuthority.sol";
+import './ITREXImplementationAuthority.sol';
 
 contract TREXImplementationAuthority is ITREXImplementationAuthority, Ownable {
     event UpdatedTokenImplementation(address tokenImplem);
