03ec72fb64d955d145cc38c22d75a7d460dfadf0
Joachim <joachim@tokeny.com>
2021-07-02 17:51:18 +0200

    :test:() fix test case for updated version

--- test/tokenTransfer.test.js
+++ test/tokenTransfer.test.js
@@ -141,7 +141,7 @@ contract('Token', (accounts) => {
 
   it('version returns the version of the token', async () => {
     const version1 = await token.version().should.be.fulfilled;
-    version1.toString().should.equal('3.4.0');
+    version1.toString().should.equal('3.5.0');
   });
 
   it('onchainID returns the onchainID address of the token', async () => {
