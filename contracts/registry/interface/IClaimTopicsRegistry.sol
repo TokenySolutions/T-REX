5486990a7c3e2e9e84e13eb652b7f09b3b772c31
Joachim <joachim@tokeny.com>
2022-12-15 19:27:40 +0700

    :refactor:(CTR) add cap on topics

--- contracts/registry/interface/IClaimTopicsRegistry.sol
+++ contracts/registry/interface/IClaimTopicsRegistry.sol
@@ -80,6 +80,7 @@ interface IClaimTopicsRegistry {
      * @dev Add a trusted claim topic (For example: KYC=1, AML=2).
      * Only owner can call.
      * emits `ClaimTopicAdded` event
+     * cannot add more than 15 topics for 1 token as adding more could create gas issues
      * @param _claimTopic The claim topic index
      */
     function addClaimTopic(uint256 _claimTopic) external;
