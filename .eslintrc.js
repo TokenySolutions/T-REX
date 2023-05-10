9c3e485a5b046490a73650c61c42690da140b0a4
Adam Boudj <boudjemaa.adam@gmail.com>
2023-05-11 01:10:49 +0400

    🔧 fix linter for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
+    ],
+    "node/no-missing-import": "off",
+    "node/no-extraneous-import": "off",
+    "no-unused-expressions": "off",
+    "node/no-unsupported-features/es-syntax": "off"
+  },
+};
