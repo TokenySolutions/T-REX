1cceb486396a184bcfc279155a27ec1e739fcb20
Adam Boudj <boudjemaa.adam@gmail.com>
2023-05-12 17:22:05 +0400

    :wrench: fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,15 @@ module.exports = {
     node: true,
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
+  extends: ["plugin:prettier/recommended", "plugin:node/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
     "node/no-missing-import": "off",
     "node/no-extraneous-import": "off",
     "no-unused-expressions": "off",
-    "node/no-unsupported-features/es-syntax": "off"
+    "node/no-unsupported-features/es-syntax": "off",
   },
 };
