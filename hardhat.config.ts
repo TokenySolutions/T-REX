acb070a0f01646e60d3ebba61060cee2dbc85deb
Adam Boudj <boudjemaa.adam@gmail.com>
2023-05-12 15:12:56 +0400

    :wrench: add gasReporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const config: HardhatUserConfig = {
     freshOutput: true,
   },
   gasReporter: {
-    enabled: true,
+    enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
 };
