568d1f17e21b25023fb68d8bbd7834600f2ae00b
joachimlebrun <joachim@tokeny.com>
2023-10-06 00:23:26 +0700

    :heavy_plus_sign:(gateway) add exports

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ const TREXImplementationAuthority = require('./artifacts/contracts/proxy/authori
 // factory
 const ITREXFactory = require('./artifacts/contracts/factory/ITREXFactory.sol/ITREXFactory.json');
 const TREXFactory = require('./artifacts/contracts/factory/TREXFactory.sol/TREXFactory.json');
+// gateway
+const ITREXGateway = require('./artifacts/contracts/factory/ITREXGateway.sol/ITREXGateway.json');
+const TREXGateway = require('./artifacts/contracts/factory/TREXGateway.sol/TREXGateway.json');
 // DVD
 const DVDTransferManager = require('./artifacts/contracts/DVD/DVDTransferManager.sol/DVDTransferManager.json');
 // compliance
@@ -102,6 +105,8 @@ module.exports = {
     TREXImplementationAuthority,
     // factory
     TREXFactory,
+    // gateway
+    TREXGateway,
     // DVD
     DVDTransferManager,
     // compliance
@@ -129,6 +134,7 @@ module.exports = {
     IIAFactory,
     ITREXImplementationAuthority,
     ITREXFactory,
+    ITREXGateway,
     IModularCompliance,
     IModule,
   },
