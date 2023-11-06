7088492cad4d269d884dd22f4a213fb76e49482b
Mihalis <Mihalis>
2023-11-06 11:42:41 +0200

    TP-2788 - Increase coverage

--- test/compliances/module-supply-limit.test.ts
+++ test/compliances/module-supply-limit.test.ts
@@ -92,6 +92,23 @@ describe('Compliance Module: SupplyLimit', () => {
     });
   });
 
+  describe('.getSupplyLimit', () => {
+    describe('when calling directly', () => {
+      it('should return', async () => {
+        const context = await loadFixture(deploySupplyLimitFixture);
+        await context.suite.compliance.callModuleFunction(
+          new ethers.utils.Interface(['function setSupplyLimit(address _compliance, uint256 _limit)']).encodeFunctionData('setSupplyLimit', [
+            context.suite.compliance.address,
+            1600,
+          ]),
+          context.suite.complianceModule.address,
+        );
+        const supplyLimit = await context.suite.complianceModule.getSupplyLimit(context.suite.compliance.address);
+        expect(supplyLimit).to.be.eq(1600);
+      });
+    });
+  });
+
   describe('.moduleCheck', () => {
     const zeroAddress = '0x0000000000000000000000000000000000000000';
     describe('when value exceeds compliance supply limit', () => {
@@ -129,6 +146,90 @@ describe('Compliance Module: SupplyLimit', () => {
 
         const result = await context.suite.complianceModule.moduleCheck(from, to, 100, context.suite.compliance.address);
         expect(result).to.be.true;
+      });
+    });
+
+    describe('.moduleTransferAction', () => {
+      describe('when calling from a random wallet', () => {
+        it('should revert', async () => {
+          const context = await loadFixture(deploySupplyLimitFullSuite);
+
+          await expect(
+            context.suite.complianceModule.moduleTransferAction(context.accounts.anotherWallet.address, context.accounts.anotherWallet.address, 10),
+          ).to.be.revertedWith('only bound compliance can call');
+        });
+      });
+
+      describe('when calling as the compliance', () => {
+        it('should do nothing', async () => {
+          const context = await loadFixture(deploySupplyLimitFullSuite);
+
+          await expect(
+            context.suite.compliance.callModuleFunction(
+              new ethers.utils.Interface(['function moduleTransferAction(address _from, address _to, uint256 _value)']).encodeFunctionData(
+                'moduleTransferAction',
+                [context.accounts.anotherWallet.address, context.accounts.anotherWallet.address, 10],
+              ),
+              context.suite.complianceModule.address,
+            ),
+          ).to.eventually.be.fulfilled;
+        });
+      });
+    });
+
+    describe('.moduleMintAction', () => {
+      describe('when calling from a random wallet', () => {
+        it('should revert', async () => {
+          const context = await loadFixture(deploySupplyLimitFullSuite);
+
+          await expect(context.suite.complianceModule.moduleMintAction(context.accounts.anotherWallet.address, 10)).to.be.revertedWith(
+            'only bound compliance can call',
+          );
+        });
+      });
+
+      describe('when calling as the compliance', () => {
+        it('should do nothing', async () => {
+          const context = await loadFixture(deploySupplyLimitFullSuite);
+
+          await expect(
+            context.suite.compliance.callModuleFunction(
+              new ethers.utils.Interface(['function moduleMintAction(address, uint256)']).encodeFunctionData('moduleMintAction', [
+                context.accounts.anotherWallet.address,
+                10,
+              ]),
+              context.suite.complianceModule.address,
+            ),
+          ).to.eventually.be.fulfilled;
+        });
+      });
+    });
+
+    describe('.moduleBurnAction', () => {
+      describe('when calling from a random wallet', () => {
+        it('should revert', async () => {
+          const context = await loadFixture(deploySupplyLimitFullSuite);
+
+          await expect(context.suite.complianceModule.moduleBurnAction(context.accounts.anotherWallet.address, 10)).to.be.revertedWith(
+            'only bound compliance can call',
+          );
+        });
+      });
+
+      describe('when calling as the compliance', () => {
+        it('should do nothing', async () => {
+          const context = await loadFixture(deploySupplyLimitFullSuite);
+
+          await expect(
+            context.suite.compliance.callModuleFunction(
+              new ethers.utils.Interface(['function moduleBurnAction(address, uint256)']).encodeFunctionData('moduleBurnAction', [
+                context.accounts.anotherWallet.address,
+                10,
+              ]),
+              context.suite.complianceModule.address,
+            ),
+          ).to.eventually.be.fulfilled;
+        });
       });
     });
   });
