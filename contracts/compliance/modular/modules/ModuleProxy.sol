cd90523f0d3b8fda51eb60e598477494aa10dc44
ali.arbak <hi@alinaciarbak.com.tr>
2024-03-11 17:06:08 +0300

    Fix lint issue

--- contracts/compliance/modular/modules/ModuleProxy.sol
+++ contracts/compliance/modular/modules/ModuleProxy.sol
@@ -65,5 +65,6 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract ModuleProxy is ERC1967Proxy {
+    // solhint-disable-next-line no-empty-blocks
     constructor(address implementation, bytes memory _data) ERC1967Proxy(implementation, _data) { }
 }
