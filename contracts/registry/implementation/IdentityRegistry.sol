483cd28596f945e5cb8a22245a9b0988eb0787a7
Mihalis <Mihalis>
2024-06-19 01:45:10 +0300

    TREX-121 - Modify recovery function

--- contracts/registry/implementation/IdentityRegistry.sol
+++ contracts/registry/implementation/IdentityRegistry.sol
@@ -269,10 +269,7 @@ contract IdentityRegistry is IIdentityRegistry, AgentRoleUpgradeable, IRStorage 
         uint16 _country
     ) public override onlyAgent {
         IIdentity identity = _tokenIdentityStorage.storedIdentity(_userAddress);
-        if (identity == _identity) {
-            // already registered in IRS
-            emit IdentityRegistered(_userAddress, _identity);
-        } else {
+        if (identity != _identity) {
             _tokenIdentityStorage.addIdentityToStorage(_userAddress, _identity, _country);
             emit IdentityRegistered(_userAddress, _identity);
         }
