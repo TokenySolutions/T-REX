5ee976f7a537eef58b61a365080d8caedfb37905
Philippe Gonday <philippe.g@tokeny.com>
2024-06-27 14:06:43 +0200

    🔧() TREX-122 Update compiler version

--- contracts/_testContracts/MockContract.sol
+++ contracts/_testContracts/MockContract.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.17;
+pragma solidity 0.8.26;
 
 contract MockContract {
     address _irRegistry;
--- contracts/roles/permissioning/owner/OwnerRolesUpgradeable.sol
+++ contracts/roles/permissioning/owner/OwnerRolesUpgradeable.sol
@@ -61,7 +61,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
