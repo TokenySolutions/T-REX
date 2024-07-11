81ca8b591a2f2ee532eadd8b16db0c667ccfe51e
Philippe Gonday <philippe.g@tokeny.com>
2024-07-11 10:03:01 +0200

    ✨() TREX-129 Default allowance

--- contracts/token/TokenStorage.sol
+++ contracts/token/TokenStorage.sol
@@ -93,9 +93,11 @@ contract TokenStorage {
 
     mapping(address => TokenRoles) internal _agentsRestrictions;
 
+    mapping(address => bool) internal _defaultAllowances;
+
     /**
      * @dev This empty reserved space is put in place to allow future versions to add new
      * variables without shifting down storage in the inheritance chain.
      */
-    uint256[48] private __gap;
+    uint256[47] private __gap;
 }
