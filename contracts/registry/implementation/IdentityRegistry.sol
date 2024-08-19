16ff9b7352f8e34db023cea8fccb76e25c4560f9
joachimlebrun <joachim@tokeny.com>
2024-08-20 00:22:09 +0800

    ✨(IR) optional eligibility checks

--- contracts/registry/implementation/IdentityRegistry.sol
+++ contracts/registry/implementation/IdentityRegistry.sol
@@ -73,6 +73,11 @@ import "../interface/IIdentityRegistryStorage.sol";
 import "../storage/IRStorage.sol";
 import "../../errors/InvalidArgumentErrors.sol";
 
+/// error triggered when eligibility checks are disabled and the disable function is called
+error EligibilityChecksDisabledAlready();
+
+/// error triggered when eligibility checks are enabled and the enable function is called
+error EligibilityChecksEnabledAlready();
 
 contract IdentityRegistry is IIdentityRegistry, AgentRoleUpgradeable, IRStorage {
 
@@ -98,9 +103,11 @@ contract IdentityRegistry is IIdentityRegistry, AgentRoleUpgradeable, IRStorage 
         _tokenTopicsRegistry = IClaimTopicsRegistry(_claimTopicsRegistry);
         _tokenIssuersRegistry = ITrustedIssuersRegistry(_trustedIssuersRegistry);
         _tokenIdentityStorage = IIdentityRegistryStorage(_identityStorage);
+        _checksDisabled = false;
         emit ClaimTopicsRegistrySet(_claimTopicsRegistry);
         emit TrustedIssuersRegistrySet(_trustedIssuersRegistry);
         emit IdentityStorageSet(_identityStorage);
+        emit EligibilityChecksEnabled();
         __Ownable_init();
     }
 
@@ -168,10 +175,29 @@ contract IdentityRegistry is IIdentityRegistry, AgentRoleUpgradeable, IRStorage 
     }
 
     /**
+     *  @dev See {IIdentityRegistry-disableEligibilityChecks}.
+     */
+    function disableEligibilityChecks() external override onlyOwner {
+        require(!_checksDisabled, EligibilityChecksDisabledAlready());
+        _checksDisabled = true;
+        emit EligibilityChecksDisabled();
+    }
+
+    /**
+     *  @dev See {IIdentityRegistry-enableEligibilityChecks}.
+     */
+    function enableEligibilityChecks() external override onlyOwner {
+        require(_checksDisabled, EligibilityChecksEnabledAlready());
+        _checksDisabled = false;
+        emit EligibilityChecksEnabled();
+    }
+
+    /**
      *  @dev See {IIdentityRegistry-isVerified}.
      */
     // solhint-disable-next-line code-complexity
     function isVerified(address _userAddress) external view override returns (bool) {
+        if(_checksDisabled) {return true;}
         if (address(identity(_userAddress)) == address(0)) {return false;}
         uint256[] memory requiredClaimTopics = _tokenTopicsRegistry.getClaimTopics();
         if (requiredClaimTopics.length == 0) {
